8af4376d4b2ba7b2060fea58deabdbf1217b04de
Luis Jesus <luis.j.fcfm@gmail.com>
2025-07-24 19:25:58 -0600

    App de mensajes estatica

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+              'https://i.pinimg.com/736x/da/c4/f3/dac4f3451c812d1c77ab6989bd925745.jpg',
+            ),
+          ),
+        ),
+        title: Text('Mi amor'),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return (index % 2 == 0)
+                      ? const HerMessageBubble()
+                      : const MyMessageBubble();
+                },),),
+
+            //Caja de Texto
+
+            const MessageFieldBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
